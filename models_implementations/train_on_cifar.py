ebf73992983245a21424d5cce1854249ffba091d
Sergiu Abed s295149 <s295149@studenti.polito.it>
2023-04-13 22:36:16 +0200

    updated train_on_cifar.py from Riccardo's repo

--- models_implementations/train_on_cifar.py
+++ models_implementations/train_on_cifar.py
@@ -9,17 +9,12 @@ from typing import Tuple
 from torchvision import transforms
 from torchvision.datasets import CIFAR100, CIFAR10
 
-from .utils import save_model, load_model
-#from utils import save_model, load_model, model_size
-
-
+from aml_project.code.utils import save_model, load_model
 
 DEVICE = "cuda"
-#DEVICE = "cpu"
 DATA_PATH = os.getcwd()
 
 BATCH_SIZE = 128
-#BATCH_SIZE = 8
 VAL_RATIO = 0.2  # Fraction of the training set used for validation
 NUM_WORKERS = 4
 
@@ -36,7 +31,6 @@ def _data_processing(dataset: CIFAR10) -> Tuple[DataLoader, DataLoader, DataLoad
             ),  # Normalizes tensor with mean and standard deviation
         ]
     )
-    
     # Define transforms for the evaluation phase
     eval_transform = train_transform
 
@@ -54,9 +48,6 @@ def _data_processing(dataset: CIFAR10) -> Tuple[DataLoader, DataLoader, DataLoad
     shuffled_indexes = torch.randperm(tr_data_len)
 
     # Partition indexes
-    # train_indexes = shuffled_indexes[0:100]
-    # val_indexes = shuffled_indexes[100:200]
-    #test_dataset = Subset(test_dataset, [i for i in range(200)])
     train_indexes = shuffled_indexes[0: int(tr_data_len * (1 - VAL_RATIO))]
     val_indexes = shuffled_indexes[int(tr_data_len * (1 - VAL_RATIO)): tr_data_len]
 
@@ -100,7 +91,11 @@ def _training(
         
     # Load checkpoint if present
     if os.path.isfile(file_path + "/checkpoint.pth"):
-        _, epoch, lr = load_model(net, file_path + "/checkpoint.pth")
+        data = load_model(file_path + "/checkpoint.pth")
+        epoch = data["epoch"]
+        lr = data["lr"]
+        weights = data["weights"]
+        net.load_state_dict(weights)
         cur_epoch = epoch
     else:
         cur_epoch = 0
@@ -112,7 +107,6 @@ def _training(
     criterion = nn.CrossEntropyLoss()
 
     # Choose parameters to optimize
-    #parameters_to_optimize = net.parameters()
     parameters_to_optimize = [p for p in net.parameters() if p.requires_grad]
 
     # Define optimizer
@@ -133,10 +127,6 @@ def _training(
     # Train
     current_step = 0
     max_accuracy = 0
-
-    #accuracies = []
-    #losses = []
-
     for epoch in range(cur_epoch, num_epochs):
         print(
             "Starting epoch {}/{}, LR = {}".format(
@@ -156,7 +146,7 @@ def _training(
             optimizer.zero_grad()  # Zero-ing the gradients
 
             # Forward pass to the network
-            outputs = net(images)            
+            outputs = net(images)
 
             # Compute loss based on output and ground truth
             loss = criterion(outputs, labels)
@@ -173,12 +163,10 @@ def _training(
 
         # Compute and log the average loss over all batches
         avg_loss = sum_losses.item() / len(tr_set)
-        #losses.append(avg_loss)
         print(f"Current Avg Loss = {avg_loss}")
 
         # Compute validation accuracy
         acc = _validation(net, val_set)
-        #accuracies.append(acc)
         print(f"Current Val Accuracy = {acc}")
         
         # Save the best model
@@ -198,8 +186,6 @@ def _training(
 
     # Delete checkpoint
     os.remove(file_path + "/checkpoint.pth")
-
-    #return losses, accuracies
 
 def _validation(net: torch.nn.Module, val_set: DataLoader):
     # This will bring the network to GPU if DEVICE is cuda
@@ -228,10 +214,11 @@ def _validation(net: torch.nn.Module, val_set: DataLoader):
     return accuracy
 
 
-def _testing(net: nn.Module, test_set: DataLoader, model_path: str=None):
+def _testing(net: nn.Module, test_set: DataLoader, path: str=None):
     # Load model if available
-    if model_path is not None:
-        load_model(net,model_path)
+    if path is not None:
+        data = load_model(path)
+        net.load_state_dict(data["weights"])
 
     net = net.to(DEVICE)
     net.train(False)  # Set Network to evaluation mode
@@ -254,8 +241,8 @@ def _testing(net: nn.Module, test_set: DataLoader, model_path: str=None):
     accuracy = running_corrects / (len(test_set)*BATCH_SIZE)
 
     # Save model with test accuracy if path available
-    if model_path is not None:
-        save_model(net, model_path, None, accuracy, None)
+    if path is not None:
+        save_model(net, path, None, accuracy, None)
 
     print("Test Accuracy: {}".format(accuracy))
 
@@ -288,20 +275,3 @@ def train_on_cifar(
     )
 
     _testing(net, test_set, file_path + "/best_model.pth")
-    
-# if __name__ == "__main__":
-#     from shufflenetv2 import ShuffleNetV2
-    
-#     LR = 1e-1     
-#     MOMENTUM = 0.9       
-#     WEIGHT_DECAY = 1e-4  
-#     NUM_EPOCHS = 5
-#     FILE_PATH =  os.getcwd()
-#     NUM_CLASSES = 100
-#     GROUPS = 2
-#     REPEATS = [2, 4, 2]
-#     OUT_CHANNELS = [16, 32, 64, 128, 256]
-
-#     net = ShuffleNetV2(REPEATS, OUT_CHANNELS, NUM_CLASSES)
-#     print(f"Num Parameters = {model_size(net)}")
-#     train_on_cifar(net, NUM_EPOCHS, LR, MOMENTUM, WEIGHT_DECAY, cifar_100=True, file_path=FILE_PATH)
