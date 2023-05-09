731b2f53f332aa442f610054bb49e4e0341a6396
Sergiu Abed s295149 <s295149@studenti.polito.it>
2023-05-09 13:39:13 +0200

    changed local epochs to 4

--- client/client.py
+++ client/client.py
@@ -8,7 +8,7 @@ from fedmd.models_implementations.train_on_cifar import _training, _validation
 from fedmd.models_implementations.utils import load_model, save_model
 import os
 
-LOCAL_EPOCH = 5
+LOCAL_EPOCH = 4
 LR_ADAM = 0.001#0.001
 LR_SGD = 0.001
 WEIGHT_DECAY = 0.0001
